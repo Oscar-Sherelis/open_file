477ad283539377fc7ffe47def765f68347c3a073
Oskar Šerelis <oskaras34@gmail.com>
2021-03-17 15:28:37 +0200

    Initial commit

---
+++ View/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Index {
+    function go () {
+        print_r('hello');
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open file</title>
+</head>
+
+<body>
+    <input type="file">
+</body>
+
+</html>
---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+
+define("FILE_NAME", "");
